ae1eafda532c01110bcade533abcd8041f5ae02e
Satvik046 <satvikSDE0403@gmail.com>
2024-11-03 10:58:42 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
+// src/App.js
+import React, { useState } from 'react';
+import StudentDashboard from './components/StudentDashboard';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routes, Route } from 'react-router-dom';
-
-import LoginPage from './LoginPage';
-import SignupPage from './SignupPage.js';
-import HomePage from './HomePage.js';
-import StudentDashboard from './StudentDashboard';
-import CourseDetails from './CourseDetails.js';
+import LoginPage from './components/LoginPage';
+import SignupPage from './components/SignupPage.js';
+import HomePage from './components/HomePage.js';
 import './App.css';
+import InstructorDashboard from './components/InstructorDashboard';
+import CourseDetails from './components/CourseDetails';
+import Dashboard from './components/Dashboard';
 
 const App = () => {
+  const [userRole, setUserRole] = useState('student');
+
   return (
     <div className="app">
+    
       <div className="main-content">
         <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="/" element={<HomePage />} />
-          <Route path="/student-dashboard" element={<StudentDashboard />} />
-          <Route path="/coursedetails" element={<CourseDetails />} />
+        <Route path="/dashboard" element={<Dashboard userRole={userRole} />} />
+        <Route path="/login" element={<LoginPage setUserRole={setUserRole} />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/dashboard" element={<StudentDashboard />} /> {/* Route for Student Dashboard */}
+        <Route path="/" element={<StudentDashboard />} />
+        <Route path="/course/:courseId" element={<CourseDetails />} />
+        <Route path="/instructor-dashboard" component={InstructorDashboard} />
         </Routes>
       </div>
+   
     </div>
   );
 };
